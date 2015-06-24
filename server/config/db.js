6d6dfcd26f27c49f2de4398915ec9085f218aaa3
RichardStanley <rstanley943@gmail.com>
2015-06-24 16:29:00 -0700

    Trying to fix db

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,6 @@
 var knex = require('knex')({
   client: 'mysql',
-  connection: {
-    host: 'us-cdbr-iron-east-02.cleardb.net',
-    user: 'be137a0eff3840',
-    password: 'e3d9fcbc',
-    database: 'heroku_24e75f446011559',
-    charset: 'utf8'
-  }
+  connection: DATABASE_URL
 });
 
 var bookshelf = module.exports = require('bookshelf')(knex);
